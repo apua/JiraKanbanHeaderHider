893189d098d5ad13184823be05d3582886ff78d3
Apua Juan <apua.juan@sifive.com>
2023-10-03 15:36:56 +0800

    Implement hidden at the Kanban board

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,12 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+//browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+//    console.log("Received response: ", response);
+//});
+//
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//    console.log("Received request: ", request);
+//});
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+setTimeout(() => {
+    document.querySelector('.css-1qpaaev').toggleAttribute('hidden');
+    console.log('toggle hidden to .css-1qpaaev');
+}, 1000);
