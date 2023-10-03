2274787426a235f2b677dcf3b8f3612de4df9ea4
Apua Juan <apua.juan@sifive.com>
2023-10-03 12:29:43 +0800

    Initial Commit

---
+++ iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Apua Juan on 2023/10/3.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
